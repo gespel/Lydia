c64d7eeb73335b917cc6f3b0d7a0890c93657417
stenh <sten.heimbrodt@hotmail.de>
2023-07-03 16:43:42 +0200

    misc

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Lydia',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='stenh',
+    author_email='',
+    description=''
+)
