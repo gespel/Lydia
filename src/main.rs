5903dc12815b678005d38dce7bd36a8f859e02e0
gespel <sten.heimbrodt@hotmail.de>
2026-04-02 11:08:20 +0200

    i love rust

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use ssh2::Session;
-use tokio::sync::RwLockMappedWriteGuard;
-use std::{net::TcpStream, thread::JoinHandle};
+use core::time;
+use std::{net::TcpStream, thread};
 use colored::Colorize;
 use std::io::Write;
 use chrono::Local;
@@ -24,18 +24,27 @@ fn check_ssh_login(host: &str, port: u16, username: &str, password: &str) -> boo
             let mut sess = Session::new().expect("Failed to create SSH session");
 
             sess.set_tcp_stream(tcp);
-            sess.handshake().map_err(|e| format!("SSH handshake failed: {}", e)).expect("SSH handshake failed!");
-
-            match sess.userauth_password(username, password) {
+            match sess.handshake() {
                 Ok(_) => {
-                    log::info!("Login found! target: {} user: {} password: {}", host.green(), username.green(), password.green());
-                    sess.authenticated()
+                    match sess.userauth_password(username, password) {
+                        Ok(_) => {
+                            log::info!("Login found! target: {} user: {} password: {}", host.green(), username.green(), password.green());
+                            sess.authenticated()
+                        },
+                        Err(e) => {
+                            log::info!("Login failed! target: {} user: {} password: {} {}", host.red(), username.red(), password.red(), e.message().purple().italic());
+                            false
+                        },
+                    }
                 },
                 Err(e) => {
-                    log::info!("Login failed! target: {} user: {} password: {} {}", host.red(), username.red(), password.red(), e.message().purple().italic());
+                    log::error!("SSH handshake failed: {}", e);
+                    thread::sleep(time::Duration::from_millis(10000));
                     false
-                },
+                }
             }
+
+            
         },
         Err(e) => {
             println!("Unable to connect to target! {:?}", e);
@@ -70,6 +79,6 @@ async fn main() {
     create_attack_handle("sten-heimbrodt.de".to_string(), 22);
 
     loop {
-        
+
     }
 }
